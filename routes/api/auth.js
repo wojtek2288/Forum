27680f8cb9d8ca79c4eee12040fdbbb5e7192c41
Wojciech <wojtek2288@gmail.com>
2021-09-30 11:52:09 +0200

    Add Profile API routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
+
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
