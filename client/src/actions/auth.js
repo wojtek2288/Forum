8eff4065a5da2f9c2fe2d45afec682e0f76f8511
Wojciech <wojtek2288@gmail.com>
2021-10-10 13:40:11 +0200

    Add profile dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_FAIL,
   LOGOUT,
   LOGOUT_FAIL,
+  CLEAR_PROFILE,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -96,5 +97,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout / Clear profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,6 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGIN';
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
